398c32b62e490b18d37e132d5e8d06b72131b5a5
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-12 09:26:53 -0600

    Modified NAT rule

--- lb.tf
+++ lb.tf
@@ -1,10 +1,11 @@
+# VIP address
 resource "azurerm_public_ip" "lbIP" {
   name                         = "LBPublicIP"
   location                     = "${azurerm_resource_group.ResourceGrps.location}"
   resource_group_name          = "${azurerm_resource_group.ResourceGrps.name}"
   public_ip_address_allocation = "static"
 }
-
+# Front End Load Balancer
 resource "azurerm_lb" "LB" {
   name                = "WebLoadBalancer"
   location            = "${azurerm_resource_group.ResourceGrps.location}"
@@ -15,7 +16,7 @@ resource "azurerm_lb" "LB" {
     public_ip_address_id = "${azurerm_public_ip.lbIP.id}"
   }
 }
-
+# Back End Address Pool
 resource "azurerm_lb_backend_address_pool" "web" {
   location            = "${azurerm_resource_group.ResourceGrps.location}"
   resource_group_name = "${azurerm_resource_group.ResourceGrps.name}"
@@ -23,32 +24,16 @@ resource "azurerm_lb_backend_address_pool" "web" {
   name                = "BackEndAddressPool"
 }
 
+# NAT Rules
 resource "azurerm_lb_nat_rule" "Rules" {
+  count = "${var.count}"
   location                       = "${azurerm_resource_group.ResourceGrps.location}"
   resource_group_name            = "${azurerm_resource_group.ResourceGrps.name}"
   loadbalancer_id                = "${azurerm_lb.LB.id}"
   name                           = "rdp_access"
   protocol                       = "tcp"
-  frontend_port                  = 3389
-  backend_port                   = 3389
-  frontend_ip_configuration_name = "PublicIPAddress"
-}
-
-resource "azurerm_lb_rule" "Rule1" {
-  location                       = "${azurerm_resource_group.ResourceGrps.location}"
-  resource_group_name            = "${azurerm_resource_group.ResourceGrps.name}"
-  loadbalancer_id                = "${azurerm_lb.LB.id}"
-  name                           = "LBRule"
-  protocol                       = "Tcp"
   frontend_port                  = 54389
   backend_port                   = 3389
   frontend_ip_configuration_name = "PublicIPAddress"
 }
 
-resource "azurerm_lb_probe" "Probe1" {
-  location            = "${azurerm_resource_group.ResourceGrps.location}"
-  resource_group_name = "${azurerm_resource_group.ResourceGrps.name}"
-  loadbalancer_id     = "${azurerm_lb.LB.id}"
-  name                = "rdp_running_probe"
-  port                = 3389
-}
--- nic.tf
+++ nic.tf
@@ -3,31 +3,14 @@ resource "azurerm_network_interface" "nics" {
   name                      = "vmnic-web-0${count.index + 1}"
   location                  = "${azurerm_resource_group.ResourceGrps.location}"
   resource_group_name       = "${azurerm_resource_group.ResourceGrps.name}"
-  network_security_group_id = "${azurerm_network_security_group.web_fw.id}"
+  #network_security_group_id = "${azurerm_network_security_group.web_fw.id}"
 
   ip_configuration {
     name                                    = "ipconfig${count.index +1}"
     subnet_id                               = "${azurerm_subnet.subnet1.id}"
     private_ip_address_allocation           = "dynamic"
-    #public_ip_address_id                    = "${azurerm_public_ip.lbIP.ip_address}"
     load_balancer_backend_address_pools_ids = ["${azurerm_lb_backend_address_pool.web.id}"]
   }
 }
 
-resource "azurerm_network_security_group" "web_fw" {
-  name                = "nsg_web"
-  location            = "${azurerm_resource_group.ResourceGrps.location}"
-  resource_group_name = "${azurerm_resource_group.ResourceGrps.name}"
 
-  security_rule {
-    name                       = "allow-rdp"
-    priority                   = 1000
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
