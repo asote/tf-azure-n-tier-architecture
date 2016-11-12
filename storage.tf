1d79060958048429a25521524687357b83b4890f
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-11 23:40:29 -0600

    Changed storage acc name

--- storage.tf
+++ storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "stacc2" {
-  name                = "webserverstorage"
+  name                = "asotwebserverstor"
   resource_group_name = "${azurerm_resource_group.ResourceGrps.name}"
 
   location     = "${azurerm_resource_group.ResourceGrps.location}"
