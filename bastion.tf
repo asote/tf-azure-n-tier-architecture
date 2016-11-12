5de0c5d04e2fc3d6fcb5adfbbfc8c928c4b786cc
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-12 11:02:58 -0600

    Initial commit

--- bastion.tf
+++ bastion.tf
@@ -1,4 +1,3 @@
-
 variable "admin_username" {}
 
 variable "admin_password" {}
@@ -62,12 +61,13 @@ resource "azurerm_network_interface" "bastionnics" {
   #network_security_group_id = "${azurerm_network_security_group.web_fw.id}"
 
   ip_configuration {
-    name                                    = "ipconfig${count.index +1}"
-    subnet_id                               = "${azurerm_subnet.subnet1.id}"
-    private_ip_address_allocation           = "dynamic"
-    public_ip_address_id  = "${azurerm_public_ip.PublicIP.id}"
+    name                          = "ipconfig${count.index +1}"
+    subnet_id                     = "${azurerm_subnet.subnet1.id}"
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.PublicIP.id}"
   }
 }
+
 resource "azurerm_public_ip" "PublicIP" {
   name                         = "BastionPublicIP"
   location                     = "${azurerm_resource_group.ResourceGrps.location}"
