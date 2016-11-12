86d3117d51b4186a2507f46abb2923fc322299b3
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-12 10:28:54 -0600

    Removed NAT rule , added LB rule

--- lb.tf
+++ lb.tf
@@ -26,26 +26,22 @@ resource "azurerm_lb_backend_address_pool" "web" {
   name                = "BackEndAddressPool"
 }
 
-# NAT Rules
-resource "azurerm_lb_nat_pool" "RDP" {
-  location                       = "${azurerm_resource_group.ResourceGrps.location}"
-  resource_group_name            = "${azurerm_resource_group.ResourceGrps.name}"
-  loadbalancer_id                = "${azurerm_lb.LB.id}"
-  name                           = "RDPApplication_Pool"
-  protocol                       = "Tcp"
-  frontend_port_start            = 54389
-  frontend_port_end              = 54489
-  backend_port                   = 3389
+# Load Balancer Rule
+resource "azurerm_lb_rule" "LBRule" {
+  location = "${azurerm_resource_group.ResourceGrps.location}"
+  resource_group_name = "${azurerm_resource_group.ResourceGrps.name}"
+  loadbalancer_id = "${azurerm_lb.LB.id}"
+  name = "HTTPRule"
+  protocol = "Tcp"
+  frontend_port = 80
+  backend_port = 80
   frontend_ip_configuration_name = "PublicIPAddress"
 }
 
-resource "azurerm_lb_nat_rule" "Rules" {
-  location                       = "${azurerm_resource_group.ResourceGrps.location}"
-  resource_group_name            = "${azurerm_resource_group.ResourceGrps.name}"
-  loadbalancer_id                = "${azurerm_lb.LB.id}"
-  name                           = "rdp_access"
-  protocol                       = "tcp"
-  frontend_port                  = 3389
-  backend_port                   = 3389
-  frontend_ip_configuration_name = "PublicIPAddress"
+resource "azurerm_lb_probe" "LBProbe" {
+  location = "${azurerm_resource_group.ResourceGrps.location}"
+  resource_group_name = "${azurerm_resource_group.ResourceGrps.name}"
+  loadbalancer_id = "${azurerm_lb.LB.id}"
+  name = "HTTP_Running_Probe"
+  port = 80
 }
