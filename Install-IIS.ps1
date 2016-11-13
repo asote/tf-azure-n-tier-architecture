a0d9e59f1f69936efffa14bffeb9fd2a9339ffd1
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-12 22:45:08 -0600

    Modified PS script

--- Install-IIS.ps1
+++ Install-IIS.ps1
@@ -1,2 +1,13 @@
+# Install IIS
 import-module ServerManager
 Add-WindowsFeature Web-Server,web-management-console
+
+# Initialize Partition and Format New Disk
+Get-Disk | Where {$_.partitionstyle -eq 'raw'} | `
+Initialize-Disk -PartitionStyle MBR -PassThru | `
+New-Partition -AssignDriveLetter -UseMaximumSize | `
+Format-Volume -FileSystem NTFS -NewFileSystemLabel "data" -Confirm:$false
+
+# Create default.html
+New-Item -ItemType "file" -Path "C:\inetpub\wwwroot\default.html"
+Add-Content C:\inetpub\wwwroot\default.html $env:COMPUTERNAME
