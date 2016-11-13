8df684cfa9ab53b0484def976e4a528edec3a172
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-13 08:45:28 -0600

    Removed provisioners

--- vm.tf
+++ vm.tf
@@ -52,27 +52,4 @@ resource "azurerm_virtual_machine" "vmtest" {
     enable_automatic_upgrades = "false"
     provision_vm_agent        = "false"
   }
-  provisioner "file" {
-    source      = "Install-IIS.ps1"
-    destination = "c:\\Install-IIS.ps1"
-
-    connection {
-      type     = "winrm"
-      user     = "${var.admin_username}"
-      password = "${var.admin_password}"
-      timeout  = "30m"
-    }
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "powershell.exe -sta -ExecutionPolicy Unrestricted -file C:\\Install-IIS.ps1",
-    ]
-
-    connection {
-      type     = "winrm"
-      user     = "${var.admin_username}"
-      password = "${var.admin_password}"
-      timeout  = "30m"
-    }
-  }
 }
