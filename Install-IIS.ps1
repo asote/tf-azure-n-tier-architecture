b955b74128a2d5b05658528afe763e4b50b10e7a
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-13 21:31:48 -0600

    fixed script

--- Install-IIS.ps1
+++ Install-IIS.ps1
@@ -2,7 +2,7 @@
 get-WindowsFeature -name Web-Server | Install-WindowsFeature -IncludeManagementTools
 
 # Initialize Partition and Format New Disk
-Get-Disk | ? {$_.PartitionStyle -eq "RAW"} | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | `
+Get-Disk | ? {$_.PartitionStyle -eq "RAW"} | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize
 Format-Volume -Drive F -FileSystem NTFS -NewFileSystemLabel data -Confirm:$false
 
 
