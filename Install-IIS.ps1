29c5a556f2927520657e019ddf1e61afac26628e
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-13 21:27:26 -0600

    Fixed script

--- Install-IIS.ps1
+++ Install-IIS.ps1
@@ -2,10 +2,10 @@
 get-WindowsFeature -name Web-Server | Install-WindowsFeature -IncludeManagementTools
 
 # Initialize Partition and Format New Disk
-Get-Disk -Number 2 | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter F -UseMaximumSize
-Format-Volume -DriveLetter F -FileSystem NTFS -NewFileSystemLabel data -Confirm:$false
+Get-Disk | ? {$_.PartitionStyle -eq "RAW"} | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | `
+Format-Volume -Drive F -FileSystem NTFS -NewFileSystemLabel data -Confirm:$false
 
 
 # Create default.html
 New-Item -ItemType "file" -Path "C:\inetpub\wwwroot\default.htm"
-Add-Content C:\inetpub\wwwroot\default.html $env:COMPUTERNAME
+Add-Content C:\inetpub\wwwroot\default.htm $env:COMPUTERNAME
