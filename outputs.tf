8ca1dedc934849fad5bd060570a9e9938d2e50b2
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-15 07:41:20 -0600

    Added 2nd tier

---
+++ outputs.tf
@@ -0,0 +1,25 @@
+output "webservers_name" {
+  value = ["${azurerm_virtual_machine.tier1-vm.*.name}"]
+}
+
+output "sqlservers_name" {
+  value = ["${azurerm_virtual_machine.tier2-vm.*.name}"]
+}
+output "bastion_name" {
+  value = ["${azurerm_virtual_machine.jumphost.*.name}"]
+}
+
+output "bastion_ip" {
+  value = ["${azurerm_network_interface.bastionnics.private_ip_address}"]
+}
+
+output "webservers_ip" {
+  value = ["${azurerm_network_interface.tier1-nics.*.private_ip_address}"]
+}
+output "sqlservers_ip" {
+  value = ["${azurerm_network_interface.tier2-nics.*.private_ip_address}"]
+}
+
+output "LB VIP" {
+  value = ["${azurerm_public_ip.lbIP.ip_address}"]
+}
