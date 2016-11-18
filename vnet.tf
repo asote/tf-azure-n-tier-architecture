60a2f32d930c2a01e3ae8bd1f34446ee016f8ebb
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-17 20:03:06 -0600

    Fixed subne reosurces

--- vnet.tf
+++ vnet.tf
@@ -9,41 +9,41 @@ resource "azurerm_virtual_network" "vnet1" {
 
 # Create subnets
 resource "azurerm_subnet" "subnet1" {
-  name                 = "web_tier"
-  resource_group_name  = "${azurerm_resource_group.ResourceGrps.name}"
-  virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
-  address_prefix       = "10.0.1.0/24"
-  security_group       = "${azurerm_network_security_group.tier1_fw.id}"
+  name                      = "web_tier"
+  resource_group_name       = "${azurerm_resource_group.ResourceGrps.name}"
+  virtual_network_name      = "${azurerm_virtual_network.vnet1.name}"
+  address_prefix            = "10.0.1.0/24"
+  network_security_group_id = "${azurerm_network_security_group.tier1_fw.id}"
 }
 
 resource "azurerm_subnet" "subnet2" {
-  name                 = "business_tier"
-  resource_group_name  = "${azurerm_resource_group.ResourceGrps.name}"
-  virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
-  address_prefix       = "10.0.2.0/24"
-  security_group       = "${azurerm_network_security_group.tier2_fw.id}"
+  name                      = "business_tier"
+  resource_group_name       = "${azurerm_resource_group.ResourceGrps.name}"
+  virtual_network_name      = "${azurerm_virtual_network.vnet1.name}"
+  address_prefix            = "10.0.2.0/24"
+  network_security_group_id = "${azurerm_network_security_group.tier2_fw.id}"
 }
 
 resource "azurerm_subnet" "subnet3" {
-  name                 = "data_tier"
-  resource_group_name  = "${azurerm_resource_group.ResourceGrps.name}"
-  virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
-  address_prefix       = "10.0.3.0/24"
-  security_group       = "${azurerm_network_security_group.tier3_fw.id}"
+  name                      = "data_tier"
+  resource_group_name       = "${azurerm_resource_group.ResourceGrps.name}"
+  virtual_network_name      = "${azurerm_virtual_network.vnet1.name}"
+  address_prefix            = "10.0.3.0/24"
+  network_security_group_id = "${azurerm_network_security_group.tier3_fw.id}"
 }
 
 resource "azurerm_subnet" "subnet4" {
-  name                 = "ADDS_net"
-  resource_group_name  = "${azurerm_resource_group.ResourceGrps.name}"
-  virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
-  address_prefix       = "10.0.4.0/27"
-  security_group       = "${azurerm_network_security_group.tier4_fw.id}"
+  name                      = "ADDS_net"
+  resource_group_name       = "${azurerm_resource_group.ResourceGrps.name}"
+  virtual_network_name      = "${azurerm_virtual_network.vnet1.name}"
+  address_prefix            = "10.0.4.0/27"
+  network_security_group_id = "${azurerm_network_security_group.tier4_fw.id}"
 }
 
 resource "azurerm_subnet" "subnet5" {
-  name                 = "management_net"
-  resource_group_name  = "${azurerm_resource_group.ResourceGrps.name}"
-  virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
-  address_prefix       = "10.0.0.128/25"
-  security_group       = "${azurerm_network_security_group.tier5_fw.id}"
+  name                      = "management_net"
+  resource_group_name       = "${azurerm_resource_group.ResourceGrps.name}"
+  virtual_network_name      = "${azurerm_virtual_network.vnet1.name}"
+  address_prefix            = "10.0.0.128/25"
+  network_security_group_id = "${azurerm_network_security_group.tier5_fw.id}"
 }
