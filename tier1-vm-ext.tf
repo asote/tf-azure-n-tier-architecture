07196bb34d8eb7e448f3ed4157b97d13737cb54f
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-20 15:07:42 -0600

    Added DSC  zip file

--- tier1-vm-ext.tf
+++ tier1-vm-ext.tf
@@ -3,6 +3,8 @@
 # https://blogs.endjin.com/2015/07/using-azure-resource-manager-and-powershell-dsc-to-create-and-provision-a-vm/
 #
 # https://docs.microsoft.com/en-us/azure/virtual-machines/virtual-machines-windows-extensions-dsc-overview
+# Publish-AzureRmVMDscConfiguration ".\IIS-Install.ps1" -OutputArchivePath ".\IIS-Install.ps1.zip"
+
 
 resource "azurerm_virtual_machine_extension" "DSC" {
   count                = "3"
@@ -16,8 +18,7 @@ resource "azurerm_virtual_machine_extension" "DSC" {
 
   settings = <<SETTINGS
   {
-    "modulesUrl"      : "https://asotelostor.blob.core.windows.net/files/IIS-Install.ps1",
-    "script"   : "IIS-Install.ps1",
+    "modulesUrl"      : "https://asotelostor.blob.core.windows.net/files/IIS-Install.ps1.zip",
     "sastoken" : "?sv=2015-04-05&ss=b&srt=o&sp=ra&se=2017-01-31T22:53:59Z&st=2016-11-20T14:53:59Z&spr=https&sig=4LTq%2FXvoqWXb1Wr0vWGy8uK%2FGTR5ix78SJrsfkspW%2FA%3D",
     "configurationFunction" : "IIS-Install.ps1\\IISInstall"
     "wmfVersion" : "latest"
