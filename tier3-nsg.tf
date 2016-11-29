38f92b1125f73997da814a9b5b016ed5ee6b71df
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-28 21:38:43 -0600

    modified nsg rules

--- tier3-nsg.tf
+++ tier3-nsg.tf
@@ -12,42 +12,42 @@ resource "azurerm_network_security_group" "tier3_fw" {
     source_port_range          = "*"
     destination_port_range     = "1433"
     source_address_prefix      = "10.0.2.0/24"
-    destination_address_prefix = "10.0.3.0/24"
-  }
-
-  security_rule {
-    name                       = "allow-winrm"
-    priority                   = 101
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "5985-5986"
-    source_address_prefix      = "*"
-    destination_address_prefix = "10.0.3.0/24"
+    destination_address_prefix = "*"
   }
 
   security_rule {
     name                       = "allow-RDP"
-    priority                   = 102
+    priority                   = 110
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "3389"
     source_address_prefix      = "10.0.3.0/24"
-    destination_address_prefix = "10.0.0.128/25"
+    destination_address_prefix = "*"
   }
 
   security_rule {
-    name                       = "Deny-HTTP"
+    name                       = "Deny-All"
     priority                   = 200
     direction                  = "Inbound"
     access                     = "Deny"
-    protocol                   = "Tcp"
-    source_port_range          = "80"
-    destination_port_range     = "80"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
     source_address_prefix      = "*"
-    destination_address_prefix = "10.0.3.0/24"
+    destination_address_prefix = "*"
+  }
+
+    security_rule {
+    name                       = "Deny-All"
+    priority                   = 210
+    direction                  = "Outbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
