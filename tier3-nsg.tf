3ec3eff7aae87a9559e96fda2c6333b8a28fcf0c
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-28 22:26:21 -0600

    fixed nsg rules

--- tier3-nsg.tf
+++ tier3-nsg.tf
@@ -24,30 +24,78 @@ resource "azurerm_network_security_group" "tier3_fw" {
     source_port_range          = "*"
     destination_port_range     = "3389"
     source_address_prefix      = "10.0.3.0/24"
+    destination_address_prefix = "10.0.0.128/25"
+  }
+
+  security_rule {
+    name                       = "Deny-tier2-inTraffic"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "10.0.2.0/24"
     destination_address_prefix = "*"
   }
 
   security_rule {
-    name                       = "Deny-AllTraffic"
-    priority                   = 200
-    direction                  = "Inbound"
-    access                     = "Deny"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-    security_rule {
-    name                       = "Deny-All"
+    name                       = "Deny-tier2-outTraffic"
     priority                   = 210
     direction                  = "Outbound"
     access                     = "Deny"
-    protocol                   = "*"
+    protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "*"
+    destination_port_range     = "80"
     source_address_prefix      = "*"
+    destination_address_prefix = "10.0.2.0/24"
+  }
+
+  security_rule {
+    name                       = "Deny-tier1-inTraffic"
+    priority                   = 220
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "10.0.1.0/24"
     destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Deny-tier1-outTraffic"
+    priority                   = 230
+    direction                  = "Outbound"
+    access                     = "Deny"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "10.0.1.0/24"
+  }
+
+  security_rule {
+    name                       = "Deny-Internet-inTraffic"
+    priority                   = 240
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Deny-Internet-outTraffic"
+    priority                   = 250
+    direction                  = "Outbound"
+    access                     = "Deny"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "0.0.0.0/0"
   }
 }
