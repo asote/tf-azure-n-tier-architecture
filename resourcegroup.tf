83033634194cdc4d2fd9dc48bdc5754d04a48815
Antonio Sotelo <ASotelo6@gmail.com>
2016-12-20 23:15:41 -0600

    Set credentials as env variables

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,17 +1,5 @@
 # Configure the Azure ARM provider
-variable "ARM_SUBSCRIPTION_ID" {}
-
-variable "ARM_CLIENT_ID" {}
-
-variable "ARM_CLIENT_SECRET" {}
-
-variable "ARM_TENANT_ID" {}
-
 provider "azurerm" {
-  subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
-  client_id       = "${var.ARM_CLIENT_ID}"
-  client_secret   = "${var.ARM_CLIENT_SECRET}"
-  tenant_id       = "${var.ARM_TENANT_ID}"
 }
 
 # Create a resource group
